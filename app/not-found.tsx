b8389e3387d9a574a6aba1a50d0edad5a959ece0
Mostafa <66505876+Mostafa-El-Masry@users.noreply.github.com>
2024-03-07 18:59:43 +0300

    loading and notfound files

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex justify-center content-center h-screen">
+      <h1>Not Found Man</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
