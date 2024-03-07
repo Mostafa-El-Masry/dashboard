ba973af9a1b33a20e6c2f542e0780c0c60e04c20
Mostafa <66505876+Mostafa-El-Masry@users.noreply.github.com>
2024-03-07 18:41:31 +0300

    track upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@ import Link from "next/link";
 import Table from "./table";
 
 const Homepage = () => {
-
-  return <div>
-    <h1><Link href="/dashboard">Dashboard</Link></h1>
-    <Table />
-  </div>;
+  return (
+    <div>
+      <h1>
+        <Link href="/dashboard">Dashboard</Link>
+      </h1>
+      <Table />
+    </div>
+  );
 };
 
 export default Homepage;
