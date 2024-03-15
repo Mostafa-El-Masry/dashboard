724ccae7145f165ba0643ab38db9a419a82e1b86
Mostafa <66505876+Mostafa-El-Masry@users.noreply.github.com>
2024-03-15 21:43:22 +0300

    updating theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html data-theme="dark" lang="en">
+    <html data-theme="bumblebee" lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"],
+    themes: ["bumblebee"],
   },
 };
 export default config;
