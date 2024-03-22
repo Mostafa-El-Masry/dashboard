0b3335eeae24439e0b24c0d11614f21b8e9883bb
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-03-22 12:56:27 +0300

    Creating Api For issues

---
+++ app/api/issues/route.tsx
@@ -0,0 +1,23 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { describe } from "node:test";
+import { z } from "zod";
+
+
+const creatIssueSchema = z.object({
+    title: z.string().min(1).max(255),  
+    description: z.string().min(1)
+})
+
+export async function POST(request: NextRequest){
+    const body = await request.json();
+    const validation = creatIssueSchema.safeParse(body);
+    if(!validation.success)
+    return NextResponse.json(validation.error.errors, {status:400});
+
+    const newIssue = await prisma.issue.create({
+        data: {title: body.title, describtion: body.describtion}
+    });
+
+    return NextResponse.json(newIssue, {status : 201});
+};
