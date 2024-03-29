0c6a9b534586a32b7703049bb9f9ee4d56815c40
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-03-29 22:27:07 +0300

    Attempt to Submit Form failed 1

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,24 +1,42 @@
 "use client";
-import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import SimpleMDE from "react-simplemde-editor";
 import { Button, TextField } from "@radix-ui/themes";
 import React from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm{
+  title: String,
+  description: String,
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+const {register , control , handleSubmit} = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
+    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(async (data) => {
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    })}>
       <TextField.Root aria-placeholderer="Search the docs…">
-        <TextField.Input placeholder="title" />
+        <TextField.Input placeholder="title" {...register('title')}/>
         <TextField.Slot>
           <FaMagnifyingGlass height="16" width="16" />
         </TextField.Slot>
       </TextField.Root>
-
-      <SimpleMDE placeholder="Discribtion" className=""></SimpleMDE>
+      <Controller 
+      name="description"
+      control={control}
+      render={({field}) => <SimpleMDE placeholder="Description" {...field}/>}
+      />
+      
 
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
